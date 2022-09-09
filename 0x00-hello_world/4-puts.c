0a82072e7bb900135243224fad256ca822e1ab84
Oparaure Delight <96892410+Delight362@users.noreply.github.com>
2022-09-09 03:10:05 +0100

    update 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - using the put function to print string
+ *main - Entry point
  *
- * Description: using the main function to print
- * Programming is like building a multilingual puzzle
- * Return: 0
+ *Return: Always 0 (Success)
  */
 int main(void)
-}
-	puts("\"Programming is like building a mutilingual puzzle");
-	return (0);
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
