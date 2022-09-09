47d6baf052436f42ac5f10aab4983a445dd76011
Delight362 <oparauredu@gmail.com>
2022-09-08 18:14:29 -0700

    6-size file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,20 +5,20 @@
 *
 * Description: using the main function
 * this program prints "Programming is like building a multilingual puzzle"
-* Return: 0
+* Return: Always 0 (success)
 */
 int main(void)
 {
-	char c;
-	int i;
-	long li;
-	long long lli;
+	int a;
+	long int b;
+	long long int c;
+	char d;
 	float f;
 
-	printf("Size of a char: %id byte(s)\n", sizeof(c));
-	printf("Size of an int: %id byte(s)\n", sizeof(i));
-	printf("Size of a long int: %id byte(s)\n", sizeof(li));
-	printf("Size of a long long int: %id byte(s)\n", sizeof(lli));
-	printf("Size of a float: %id byte(s)\n", sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %id byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %id byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %id byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
