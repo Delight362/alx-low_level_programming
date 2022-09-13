d14e5cdde57253cc63d008ac227ddc164417759c
Delight362 <oparauredu@gmail.com>
2022-09-13 12:45:38 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,16 +5,16 @@
  *
  * Return : Always 0
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char alphabet;
 
-	for(alphabet='a'; alphabet <='z';alphabet++)
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 
 	{
 		_putchar(alphabet);
 	}
 
-	putchar('\n');
+	_putchar('\n');
 	return (0);
 }
