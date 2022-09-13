aff220fb5e63d32dedeeda18f123154035540ba8
Oparaure Delight <96892410+Delight362@users.noreply.github.com>
2022-09-13 19:04:33 +0100

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,12 @@
-#include <main.h>
+#include "main.h"
 /**
  * main - prints putchar
+ *
  * Return: always 0
  */
 int main(void)
 {
-	putchar('_');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -14,5 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+	
 	return (0);
 }
