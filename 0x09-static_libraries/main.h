f83e32b7b4bf4c2cccfed5d2cd53ff0d7b7e02c2
Oparaure Delight <96892410+Delight362@users.noreply.github.com>
2022-10-01 17:18:52 +0100

    main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -5,7 +5,7 @@
  * File: main.h
  * Auth: Oparaure Delight
  * Desc: Header file containing the prototypes of all functions
- *       included in the static library libholberton.a.
+ * included in the static library libholberton.a.
  */
 
 int _putchar(char c);
