918b9ae09b549562c8fcdfce8f8be70d1ccdc0ee
Oparaure Delight <96892410+Delight362@users.noreply.github.com>
2022-10-01 19:52:47 +0100

    Update main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -3,7 +3,7 @@
 
 /*
  * File: main.h
- * Auth: Oparaure Uchenna
+ * Auth: Delight Oparaure
  * Desc: Header file containing the prototypes of all functions
  * included in the static library libholberton.a.
  */
